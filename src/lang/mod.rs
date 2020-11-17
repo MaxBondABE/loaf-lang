7df42517cd5454f6ab218a63e86a0bc1b62663e5
MaxBondABE <max.o.bond@gmail.com>
2020-11-17 05:39:55 -0700

    Refactored ProgramBuilder to use builder pattern

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -8,20 +8,64 @@ pub mod parse;
 
 #[derive(Debug)]
 pub struct ProgramBuilder {
-    boundary: BoundaryBlock,
-    neighborhood: NeighborhoodBlock,
-    environment: EnvironmentBlock,
-    state: StatesBlock,
-    rule: RulesBlock
+    boundary: Option<BoundaryBlock>,
+    environment: Option<EnvironmentBlock>,
+    neighborhood: Option<NeighborhoodBlock>,
+    states: Option<StatesBlock>,
+    rules: Option<RulesBlock>,
+    valid: Option<bool>
 }
-impl ProgramBuilder {
-    pub fn new(boundary: BoundaryBlock, neighborhood: NeighborhoodBlock, environment: EnvironmentBlock, state: StatesBlock, rule: RulesBlock) -> Self {
+impl Default for ProgramBuilder {
+    fn default() -> Self {
         Self {
-            boundary,
-            neighborhood,
-            environment,
-            state,
-            rule
+            boundary: None,
+            neighborhood: None,
+            environment: None,
+            states: None,
+            rules: None,
+            valid: None
         }
     }
+}
+impl ProgramBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    // Builder methods
+    pub fn boundary(&mut self, b: BoundaryBlock) -> &mut Self {
+        self.boundary = Some(b);
+        self
+    }
+    pub fn environment(&mut self, e: EnvironmentBlock) -> &mut Self {
+        self.environment = Some(e);
+        self
+    }
+    pub fn neighborhood(&mut self, n: NeighborhoodBlock) -> &mut Self {
+        self.neighborhood = Some(n);
+        self
+    }
+    pub fn states(&mut self, s: StatesBlock) -> &mut Self {
+        self.states = Some(s);
+        self
+    }
+    pub fn rules(&mut self, r: RulesBlock) -> &mut Self {
+        self.rules = Some(r);
+        self
+    }
+    pub fn build(&self) {
+        unimplemented!()
+    }
+
+    // Validation
+    fn validate(&mut self) -> bool {
+        unimplemented!()
+    }
+    fn warnings(&self) -> Vec<Warnings> {
+        unimplemented!()
+    }
+}
+
+pub enum Warnings {
+    UnknownColor
 }
--- src/lang/parse/mod.rs
+++ src/lang/parse/mod.rs
@@ -5,6 +5,10 @@ use pest::iterators::{Pair, Pairs};
 use crate::lang::ProgramBuilder;
 use std::convert::TryInto;
 use std::num::ParseIntError;
+use crate::lang::parse::blocks::boundary::BoundaryBlock;
+use crate::lang::parse::blocks::neighborhood::NeighborhoodBlock;
+use crate::lang::parse::blocks::environment::EnvironmentBlock;
+use crate::lang::parse::blocks::state::StatesBlock;
 
 pub mod blocks;
 
@@ -16,48 +20,62 @@ pub(crate) type LoafPairs<'a> = Pairs<'a, Rule>;
 
 pub fn parse(s: &str) -> Result<ProgramBuilder, Error> {
     let root = LoafParser::parse(Rule::program, s)?;
+    let mut builder = ProgramBuilder::new();
 
-    let mut boundary_pair: Option<LoafPair> = None;
-    let mut neighborhood_pair: Option<LoafPair> = None;
-    let mut environment_pair: Option<LoafPair> = None;
-    let mut state_pair: Option<LoafPair> = None;
-    let mut rule_pair: Option<LoafPair> = None;
+    let mut parsed_boundary = false;
+    let mut parsed_neighborhood = false;
+    let mut parsed_environment = false;
+    let mut parsed_states = false;
+    let mut parsed_rules = false;
     for pair in root {
         match pair.as_rule() {
             Rule::boundary_block => {
-                boundary_pair = Some(pair)
+                if parsed_boundary {
+                    return Err(Error::MultipleDefinitionsForBlock);
+                }
+                parsed_boundary = true;
+                builder.boundary(pair.try_into()?);
             },
             Rule::neighborhood_block => {
-                neighborhood_pair = Some(pair)
+                if parsed_neighborhood {
+                    return Err(Error::MultipleDefinitionsForBlock);
+                }
+                parsed_neighborhood = true;
+                builder.neighborhood(pair.try_into()?);
             },
             Rule::environment_block => {
-                environment_pair = Some(pair)
+                if parsed_environment {
+                    return Err(Error::MultipleDefinitionsForBlock);
+                }
+                parsed_environment = true;
+                builder.environment(pair.try_into()?);
             },
             Rule::state_block => {
-                state_pair = Some(pair)
+                if parsed_states {
+                    return Err(Error::MultipleDefinitionsForBlock);
+                }
+                parsed_states = true;
+                builder.states(pair.try_into()?);
             }
             Rule::rule_block => {
-                rule_pair = Some(pair)
+                if parsed_rules {
+                    return Err(Error::MultipleDefinitionsForBlock);
+                }
+                parsed_rules = true;
+                builder.rules(pair.try_into()?);
             }
             Rule::EOI => break,
             _ => unreachable!()
         }
     }
-    Ok(
-        ProgramBuilder::new(
-            boundary_pair.unwrap().try_into()?,
-            neighborhood_pair.unwrap().try_into()?,
-            environment_pair.unwrap().try_into()?,
-            state_pair.unwrap().try_into()?,
-            rule_pair.unwrap().try_into()?
-        )
-    )
+    Ok(builder)
 }
 
 #[derive(Debug)]
 pub enum Error {
     SyntaxError(PestError<Rule>),
     UnrepresentableNumber(ParseIntError),
+    MultipleDefinitionsForBlock, // TODO include pair - triggers lifetime issues
     MultipleDefaultStates
 }
 impl From<PestError<Rule>> for Error {
