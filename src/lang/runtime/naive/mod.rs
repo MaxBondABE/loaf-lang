220f6c9b36147657462621c88185c7c341d4f505
MaxBondABE <max.o.bond@gmail.com>
2020-11-23 01:58:15 -0700

    Improved legibility of Coordinate impl
    Coordinate impl had lots of gratuitous dereferencing. However, since Coordinate is a Copy type,
    it was possible to take ownership of it's value & eliminate dereferencing.

--- src/lang/runtime/naive/mod.rs
+++ src/lang/runtime/naive/mod.rs
@@ -9,108 +9,108 @@ pub enum Coordinate {
     Coordinate3D {x: isize, y: isize, z: isize}
 }
 impl Coordinate {
-    pub fn add_x(&self, magnitude: isize) -> Self {
+    pub fn add_x(self, magnitude: isize) -> Self {
         match self {
             Coordinate1D { x } => {
                 Coordinate1D {x: x + magnitude}
             }
             Coordinate2D { x, y } => {
-                Coordinate2D { x: x + magnitude, y: *y }
+                Coordinate2D { x: x + magnitude, y}
             }
             Coordinate3D { x, y, z } => {
-                Coordinate3D { x: x + magnitude, y: *y, z: *z }
+                Coordinate3D { x: x + magnitude, y, z}
             }
         }
     }
-    pub fn add_y(&self, magnitude: isize) -> Self {
+    pub fn add_y(self, magnitude: isize) -> Self {
         match self {
             Coordinate1D { .. } => panic!("Illegal coordinate operation: 1D coordinate has no Y value"),
             Coordinate2D { x, y } => {
-                Coordinate2D { x: *x, y: y + magnitude }
+                Coordinate2D { x, y: y + magnitude }
             }
             Coordinate3D { x, y, z } => {
-                Coordinate3D { x: *x, y: y + magnitude, z: *z }
+                Coordinate3D { x, y: y + magnitude, z }
             }
         }
     }
-    pub fn add_z(&self, magnitude: isize) -> Self {
+    pub fn add_z(self, magnitude: isize) -> Self {
         match self {
             Coordinate1D { .. } => panic!("Illegal coordinate operation: 1D coordinate has no Z value"),
             Coordinate2D { .. }  => panic!("Illegal coordinate operation: 2D coordinate has no Z value"),
             Coordinate3D { x, y, z } => {
-                Coordinate3D { x: *x, y: *y, z: z + magnitude }
+                Coordinate3D { x, y, z: z + magnitude }
             }
         }
     }
-    pub fn add_all(&self, magnitude: isize) -> Vec<Coordinate> {
+    pub fn add_all(self, magnitude: isize) -> Vec<Coordinate> {
         match self {
             Coordinate::Coordinate1D { x } => {
                 vec!(Coordinate1D {x: x + magnitude})
             }
             Coordinate::Coordinate2D { x, y } => {
                 vec!(
-                    Coordinate2D {x: x + magnitude, y: *y},
-                    Coordinate2D {x: *x, y: y + magnitude}
+                    Coordinate2D {x: x + magnitude, y},
+                    Coordinate2D {x, y: y + magnitude}
                 )
             }
             Coordinate::Coordinate3D { x, y, z } => {
                 vec!(
-                    Coordinate3D {x: x + magnitude, y: *y, z: *z},
-                    Coordinate3D {x: *x, y: y + magnitude, z: *z},
-                    Coordinate3D {x: *x, y: *y, z: z + magnitude},
+                    Coordinate3D {x: x + magnitude, y, z},
+                    Coordinate3D {x, y: y + magnitude, z},
+                    Coordinate3D {x, y, z: z + magnitude},
                 )
             }
         }
     }
-    pub fn sub_x(&self, magnitude: isize) -> Self {
+    pub fn sub_x(self, magnitude: isize) -> Self {
         match self {
             Coordinate1D { x } => {
                 Coordinate1D {x: x - magnitude}
             }
             Coordinate2D { x, y } => {
-                Coordinate2D { x: x - magnitude, y: *y }
+                Coordinate2D { x: x - magnitude, y }
             }
             Coordinate3D { x, y, z } => {
-                Coordinate3D { x: x - magnitude, y: *y, z: *z }
+                Coordinate3D { x: x - magnitude, y, z }
             }
         }
     }
-    pub fn sub_y(&self, magnitude: isize) -> Self {
+    pub fn sub_y(self, magnitude: isize) -> Self {
         match self {
             Coordinate1D { .. } => panic!("Illegal coordinate operation: 1D coordinate has no Y value"),
             Coordinate2D { x, y } => {
-                Coordinate2D { x: *x, y: y - magnitude }
+                Coordinate2D { x, y: y - magnitude }
             }
             Coordinate3D { x, y, z } => {
-                Coordinate3D { x: *x, y: y - magnitude, z: *z }
+                Coordinate3D { x, y: y - magnitude, z }
             }
         }
     }
-    pub fn sub_z(&self, magnitude: isize) -> Self {
+    pub fn sub_z(self, magnitude: isize) -> Self {
         match self {
             Coordinate1D { .. } => panic!("Illegal coordinate operation: 1D coordinate has no Z value"),
             Coordinate2D { .. } => panic!("Illegal coordinate operation: 2D coordinate has no Z value"),
             Coordinate3D { x, y, z } => {
-                Coordinate3D { x: *x, y: *y, z: z - magnitude }
+                Coordinate3D { x, y, z: z - magnitude }
             }
         }
     }
-    pub fn sub_all(&self, magnitude: isize) -> Vec<Coordinate> {
+    pub fn sub_all(self, magnitude: isize) -> Vec<Coordinate> {
         match self {
             Coordinate::Coordinate1D { x } => {
                 vec!(Coordinate1D {x: x - magnitude})
             }
             Coordinate::Coordinate2D { x, y } => {
                 vec!(
-                    Coordinate2D {x: x - magnitude, y: *y},
-                    Coordinate2D {x: *x, y: y - magnitude}
+                    Coordinate2D {x: x - magnitude, y},
+                    Coordinate2D {x, y: y - magnitude}
                 )
             }
             Coordinate::Coordinate3D { x, y, z } => {
                 vec!(
-                    Coordinate3D {x: x - magnitude, y: *y, z: *z},
-                    Coordinate3D {x: *x, y: y - magnitude, z: *z},
-                    Coordinate3D {x: *x, y: *y, z: z - magnitude},
+                    Coordinate3D {x: x - magnitude, y, z},
+                    Coordinate3D {x, y: y - magnitude, z},
+                    Coordinate3D {x, y, z: z - magnitude},
                 )
             }
         }
