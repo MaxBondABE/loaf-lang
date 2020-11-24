f8e2a79015339bf1e6298ecd69f624acc5be36aa
MaxBondABE <max.o.bond@gmail.com>
2020-11-23 19:09:04 -0700

    Implemented Rules operations for Naive runtime

--- src/lang/runtime/naive/ops/mod.rs
+++ src/lang/runtime/naive/ops/mod.rs
@@ -1,4 +1,9 @@
+use std::collections::HashMap;
+
 pub mod neighborhood;
-//pub mod rules;
+pub mod rules;
 
 type StateId = usize;
+type FromState = StateId;
+type ToState = StateId;
+type StateMap = HashMap<String, StateId>;
